a6585a7940395651c98986a423776b254c59c12d
Nicholas <niyamabo@gmail.com>
2022-01-14 18:53:31 +0000

    more ui changes -  inner pannel lighter + slimed side

--- electron-project/src/App.js
+++ electron-project/src/App.js
@@ -6,15 +6,29 @@ function App() {
     <div className = "App">
       <div className = "contianer">
         <div className = "left-container">
-          <div className = "left-iner-container">
-              <h1 className = "logo">
-                vile
-
-              </h1>
-            </div>
+          <div className = "left-iner-container-top">
+            <h1 className = "logo">
+              vile
+            </h1>
+          </div>
+          <div className = "left-iner-container-bottom">
+          
+              <div type="button" className = "left-pannel-button">dahshboard</div> 
+              <div type="button" className = "left-pannel-button">analytics</div>
+              <div type="button" className = "left-pannel-button">tasks</div>
+              <div type="button" className = "left-pannel-button">proxies</div>
+              <div type="button" className = "left-pannel-button">search terms</div>
+      
+          </div>
         </div>
         <div className = "right-container">
-            <div className = "right-iner-container">
+            <div className = "right-iner-container-upper">
+              <h1 className = "logo">
+                title
+              </h1>
+
+            </div>
+            <div className = "right-iner-container-lower">
               <h1 className = "logo">
                 title
               </h1>
